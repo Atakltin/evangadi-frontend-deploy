0f175d8e5b1bf8fea81bd935cc9185c63136cac7
Atakltin <124535523+Atakltin@users.noreply.github.com>
2024-10-21 16:16:55 -0600

    some changes

--- src/pages/Question/Question.jsx
+++ src/pages/Question/Question.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import axios from "../../axiosConfig";
 import LayOut from "../../components/LayOut/LayOut";
 import Loading from "../../components/Loading/Loading";
-import PostAnswer from "../../components/PostAnswer/PostAnswer"; // Import PostAnswer component
+import PostAnswer from "../../components/PostAnswer/PostAnswer"; 
 import SingleQuestion from "../../components/SingleQuestion/SingleQuestion";
 import classes from "./Question.module.css";
 
@@ -99,14 +99,14 @@ function QuestionPage() {
                         <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
                       </svg>
                     </div>
-                    <p>{answer.username}</p> {/* Username below the SVG */}
+                    <p>{answer.username}</p> 
                   </div>
                   <p className={classes.answer__text}>{answer.answer}</p>{" "}
-                  {/* Answer content */}
+                  
                   {index < answers.length - 1 && (
                     <hr className={classes.answerDivider} />
                   )}{" "}
-                  {/* Horizontal line divider */}
+                  
                 </div>
               ))
             ) : (
